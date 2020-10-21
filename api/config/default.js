9836f4774131cd1f4ebbe92d962f4f502a6d596b
bryan kretz <kretzbryan@gmail.com>
2020-10-21 11:51:54 -0700

    webpack/mongodb refactored

---
+++ api/config/default.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://kretzbryan:iliketurtles@devconnector.jr6rb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    jwtSecret: 'dontdothat'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 console.log(path.join(__dirname, 'public'))
 module.exports = {
-    entry: './src/index.js',
+    entry: './client/src/index.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'client/public'),
         filename: 'bundle.js'
     },
     module: {
@@ -27,7 +27,7 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
+        contentBase: path.join(__dirname, 'client/public'),
         historyApiFallback: true
     }
 };
