fc2b97edf6613f536593427f5a8e63ea7369c821
bryan kretz <kretzbryan@gmail.com>
2022-01-24 20:57:50 -0800

    package updates

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+	mongoURI:
+		'mongodb+srv://kretzbryan:iliketurtles@devconnector.jr6rb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+	jwtSecret: 'dontdothat',
+	reactURI: 'http://localhost:4000',
+};
